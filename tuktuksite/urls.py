7ec76544302fc15965d5fdf6403c6425024d6fe5
Nizam Uddin Chowdhury Provat <nizamuddinchowdhury1421@gmail.com>
2026-04-26 00:10:44 +0600

    Add files via upload

---
+++ tuktuksite/urls.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', include('core.urls')),
+    path('services/', include('services.urls')),
+    path('centers/', include('centers.urls')),
+    path('agents/', include('agents.urls')),
+    path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
+    path('reports/', include('reports.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
