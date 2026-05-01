eacc48fc0136950c380ae14f311e47810ca8b963
Nizam Uddin Chowdhury Provat <nizamuddinchowdhury1421@gmail.com>
2026-05-01 18:01:41 +0600

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'core/home.html')
+
+
+def about(request):
+    return render(request, 'core/about.html')
+
+# Create your views here.
